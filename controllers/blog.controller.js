4b2ba92f139502f50fbded683048ea3a4ae3ed3f
Dinuga Weeraratne <dinuga4@gmail.com>
2020-12-15 20:26:36 +0530

    master branch changes

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -29,6 +29,21 @@ router.get('/essential-tool', (req,res)=>{
             return res.render('blog/tool', {courses:courses });
     })
 })
-
+router.get('/what-is-agile', (req,res)=>{
+    Course.find({}, (err, courses)=>{
+        if(err || courses == null) 
+            return res.status(404).json({error: "No Courses Found"})
+        
+            return res.render('blog/what-is-agile', {courses:courses });
+    })
+})
+router.get('/benifits-agile', (req,res)=>{
+    Course.find({}, (err, courses)=>{
+        if(err || courses == null) 
+            return res.status(404).json({error: "No Courses Found"})
+        
+            return res.render('blog/benifits-agile', {courses:courses });
+    })
+})
 
 module.exports = router
