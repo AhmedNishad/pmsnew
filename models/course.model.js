6327d1d330db5fe6caee15d7dbbc67e0c52bdb96
dinuga <dinuga4@gmail.com>
2021-01-08 21:49:22 +0530

    product page changes

--- models/course.model.js
+++ models/course.model.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var courseSchema = new Schema({
   title:  String, // Title at top of 
   type: String, // Agile, project management etc
-  introduction: {body: String, highlights: []}, 
+  introduction: {body: String, highlights: [], heading: String, subHeading: String}, 
   duration:   String, // Hours, days
   courseFee:   String, // USD
   courseNumber: String, 
